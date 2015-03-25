932a50179203173688078c45e1656bea2ab52cbf
Ubuntu <ubuntu@ip-10-142-247-9.ec2.internal>
2015-03-25 22:36:10 +0000

    config comments, add custom home page and include metadata in collection sync

--- install-collections.sh
+++ install-collections.sh
@@ -4,4 +4,4 @@ if [ ! -d "collections" ]; then
     mkdir collections
 fi
 
-s3cmd sync -r --exclude=* --include=cluster.idx s3://aws-publicdatasets/common-crawl/cc-index/collections/ collections/
+s3cmd sync -r --exclude=* --include="cluster.idx" --include="metadata.yaml" s3://aws-publicdatasets/common-crawl/cc-index/collections/ collections/
